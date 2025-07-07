ab8c58eecd185c4fa74c887b167ee22ded804374
uk030621 <123797901+uk030621@users.noreply.github.com>
2025-07-07 21:37:35 +0100

    Change button colour from blue to purple in contact page.

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -71,7 +71,7 @@ export default function ContactForm() {
         ></textarea>
         <button
           type="submit"
-          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+          className="bg-purple-600 text-white px-4 py-2 rounded hover:bg-blue-700"
         >
           Send Message
         </button>
